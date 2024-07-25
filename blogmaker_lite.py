c30618255eab978f1f05581d8816294c3be5fcd2
Victor <Victor.Fernandez.Pey03@gmail.com>
2024-07-25 13:45:10 +0200

    Add template directory and its functionality

--- blogmaker_lite.py
+++ blogmaker_lite.py
@@ -1,41 +1,18 @@
-from django.conf import settings
-from django.http import HttpResponse
+from pathlib import Path
 from django.urls import path
 from django.core.handlers.wsgi import WSGIHandler
-from django.core.management import \
-    execute_from_command_line
-from django.urls import path
-from pathlib import Path
-import os
-import django
 from django.shortcuts import render
+from django.contrib import admin
+from blogs.models import Blog
 
-'''
-settings.configure(
-    ROOT_URLCONF=__name__,
-    DEBUG=True,
-    SECRET_KEY="my-secret-key",
-    TEMPLATES=[
-        {
-            "BACKEND": "django.template.backends.django.DjangoTemplates",
-            "DIRS": [Path(__file__).parent / "templates"],
-        }
-    ],
-)
-'''
-# Lo anterior de settings.configure se reemplaza por lo siguiente para usar los archivos de la carpeta templates
-# Load settings.
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
-django.setup()
+admin.site.register(Blog)
 
 def index(request):
     return render(request, "index.html")
 
 urlpatterns = [
+    path("admin/", admin.site.urls),
     path("", index)
 ]
 
 application = WSGIHandler()
-
-if __name__ == "__main__":
-    execute_from_command_line()
